0ea05ab727405069c10eb9eef372d69dc7731f31
Christian <150555815+ChristoTF2@users.noreply.github.com>
2024-06-26 14:25:12 -0500

    SDA

---
+++ AgroSolutions.Domain/IAM/Services/ITokenService.cs
@@ -0,0 +1,13 @@
+using LearningCenter.Domain.IAM.Models;
+
+namespace LearningCenter.Domain.IAM.Services;
+
+public interface ITokenService
+{
+    string GenerateToken(User user);
+    
+    Task<int?> ValidateToken(string token);
+
+    string GenerateAnonymousToken();
+
+}
